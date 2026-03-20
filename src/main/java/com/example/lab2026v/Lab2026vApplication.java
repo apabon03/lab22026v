68663c110340e1daea8be9134ab5c423214a9a77
apabon03 <andres.pabonr96@gmail.com>
2026-03-20 11:34:49 -0500

    First Commit

---
+++ src/main/java/com/example/lab2026v/Lab2026vApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab2026v;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2026vApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2026vApplication.class, args);
+	}
+
+}
